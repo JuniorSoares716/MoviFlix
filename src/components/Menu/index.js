72ded700771fdd0b69c241234133b6fd0848c554
Junior Soares <34192870+JuniorSoares716@users.noreply.github.com>
2020-08-01 15:07:33 -0300

    atualização da pagina home, criaão das rotas.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 import './menu.css';
 import Button from '../Button';
@@ -7,10 +8,10 @@ import Button from '../Button';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="MoviFlix" />
-      </a>
-      <Button as="a" className="ButtonLink" href="/"> Novo Video</Button>
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video"> Novo Video</Button>
     </nav>
   );
 }
